f08f465064e001d55d58127daba7c188a8fd3ee5
ibar12345 <44119179+ibar12345@users.noreply.github.com>
2018-10-15 20:13:46 +0700

    toolv2.sh
    Selamat mencoba stah

---
+++ toolv2.sh
@@ -0,0 +1,310 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m' 
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m' 
+yellow='\033[33;1m'
+sleep 1
+echo "\033[34;1m《《《\033[36;1m========================================\033[34;1m》》》"
+echo "\033[32;1m[Author       ]: Mr.X ForAKA96                     |" 
+echo "\033[35;1m[version Tools]: v.2                               |"
+echo "\033[33;1m[Kontak       ]: 081230754052                      |"
+echo "\033[31;1m[Team         ]: NEWBIE CYBER HACKED               |"
+echo "\033[34;1m《《《\033[36;1m========================================\033[34;1m》》》"
+echo ""
+echo " \033[31;1m#Selamat mencoba stah:v😎 "
+echo " \033[33;1m#gunakan tool dengan bijak stah:v😚 "
+echo " \033[32;1m#DILARANG COLI😑 "
+echo "\033[36;1m"
+echo " [+]=========================================================[+] "
+echo $green "1.> TOOL DDOS GreenReaper "
+echo " 《《《===============================================》》》 "
+echo $purple "2.> TOOL AOC SPAMER "
+echo " 《《《===============================================》》》 "
+echo $cyan "3.> TOOL SPAM TOD "
+echo " 《《《===============================================》》》 "
+echo $red "4.> TOOL CWJSPAM "
+echo " 《《《===============================================》》》 "
+echo $white "5.> TOOL SPAM TELEPON "
+echo " 《《《===============================================》》》 "
+echo $yellow "6.> TOOL SPAM WA "
+echo " 《《《===============================================》》》 "
+echo $blue "7.> TOOL AOCDEFACE "
+echo " 《《《===============================================》》》 "
+echo $green "8.> TOOL MBF "
+echo " 《《《===============================================》》》 "
+echo $purple "9.> TOOL BLACK HYDRA "
+echo " 《《《===============================================》》》 "
+echo $cyan "10.> TOOL LAZYMUX "
+echo " 《《《===============================================》》》 "                       
+echo $red "11.> TOOL LOGIN TERMUX "
+echo " 《《《===============================================》》》 "
+echo $white "12.> TOOL FBbrute "
+echo " 《《《===============================================》》》 "
+echo $yellow "13.> TOOL IPGeolocation "
+echo " 《《《===============================================》》》 "
+echo $blue "14.> TOOL SQLMAP "
+echo " 《《《===============================================》》》 "
+echo $green "15.> TOOL BAJINGANv6 "
+echo " 《《《===============================================》》》 "
+echo $cyan "0.> keluar "
+echo " [+]=========================================================[+] "
+echo "\033[32;1m"
+read -p "root@Mr.X ForAKA96~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt upgrade
+apt update
+apt install git
+apt install python2
+git clone https://github.com/Amriez/GreenReaper.git
+cd GreenReaper
+python2 greenreaper.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade 
+pkg install git
+git clone https://github.com/rootM3eX/AOCSPAMMER
+cd AOCSPAMMER
+sh aocspam.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update
+apt upgrade
+apt install figlet
+apt install git
+git clone https://github.com/Mantoed/wew.git
+cd wew
+sh spamtod.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade
+pkg install git python python2 php toilet figlet gem
+gem install lolcat
+git clone https://github.com/CyberWestJava/CWJSPAM
+cd CWJSPAM
+sh CWJSPAM.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade
+pkg install toilet
+pkg install git
+pkg install php
+git clone https://www.github.com/Cvar1984/LiteOTP
+cd LiteOTP
+ls
+php lite.php 
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade
+pkg install git
+pkg install php
+git clone http://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sh AOC.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+pkg install python2
+pip2 install mechanize
+Pkg install git 
+git clone https:/github.com/hnov7/mbf
+cd $HOME mbf 
+python MBF.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade
+apt install git hydra
+git clone https://github.com/Gameye98/Black-Hydra/ b-hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && upgrade
+apt install python2
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96"
+apt update && apt upgrade
+pkg install nano ruby figlet python2 git -y
+gem install lolcat 
+git clone https://github.com/Harisgitama/termux-loginv2fx.git
+cd termux-loginv2fx
+python2 setup.py
+cd $HOME
+python2 useradd.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt update && apt upgrade -y
+apt install python2
+apt install php
+apt install git
+pip2 install --upgrade pip
+pip2 install mechanize
+git clone https://github.com/Senitopeng/KumpulanFbbrute.git
+cd KumpulanFbbrute
+python2 jomblo.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt-get update 
+apt-get install git 
+apt-get install python2 
+git clone http://github.com/sqlmapproject/sqlmap.git 
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt update && apt upgrade
+pip2 install requests
+git clone http://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo "\033[32;1mNEWBIE CYBER HACKED"
+sleep 2
+clear
+figlet Thanks
+sleep 2
+clear
+toilet -f future "RECODE ITU JELEK YA ASW"
+exit
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ip
+pip2 install mechanize
+git clone https://github.com/Senitopeng/KumpulanFbbrute.git
+cd KumpulanFbbrute
+python2 jomblo.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt-get update 
+apt-get install git 
+apt-get install python2 
+git clone http://github.com/sqlmapproject/sqlmap.git 
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet -f standard -F gay "Mr.X ForAKA96" 
+apt update && apt upgrade
+pip2 install request
