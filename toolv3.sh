fefcb8636cc46623adce278ff6fa420b67da96fb
ibar12345 <44119179+ibar12345@users.noreply.github.com>
2018-11-03 14:50:34 +0700

    Update toolv3.sh

--- toolv3.sh
+++ toolv3.sh
@@ -416,7 +416,7 @@ apt install python2
 apt install apache2
 apt install php
 apt install git
-git clone https://github.com/Senitopeng/Mancing.git 
+git clone https://github.com/Senitopeng/Mancin
 cd Mancing
 python2 mancing.py
 fi
